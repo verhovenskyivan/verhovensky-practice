d7f0e1079d429fa70468c08141aa41baf2727035
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-05-19 18:39:02 +0300

    Add files via upload

---
+++ RegisterfForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class RegisterfForm : Form
+    {
+        public RegisterfForm()
+        {
+            InitializeComponent();
+            userNameField.Text = "Введите имя";
+            userNameField.ForeColor = Color.Gray;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void loginFiled_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();   
+        }
+        Point lastPoint;
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            lastPoint = new Point(e.X, e.Y);
+
+        }
+
+        private void userNameField_Enter(object sender, EventArgs e)
+        {
+            if (userNameField.Text == "")
+            {
+                userNameField.Text = "Введите имя";
+                userNameField.ForeColor = Color.Black;
+            }
+        }
+
+        private void userNameField_Leave(object sender, EventArgs e)
+        {
+            {
+                if (userNameField.Text == "")
+                {
+                    userNameField.Text == "Введите имя";
+                    userNameField.ForeColor = Color.Gray;
+                }
+            }
+        }
+    }
+}
